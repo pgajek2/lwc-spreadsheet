22f61301dc8ca6e4f06910cb5267c238a60f95ab
Piotr Gajek <VMTR43@motorolasolutions.com>
2021-05-29 01:13:28 +0200

    focus, click, double

--- force-app/main/default/lwc/excelTable/excelTable.js
+++ force-app/main/default/lwc/excelTable/excelTable.js
@@ -9,14 +9,24 @@ const columns = [
     { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
 ];
 
+const CONTEXT_MENU_ACTIONS = {
+    COPY: 'copy',
+    PASTE: 'paste'
+};
+
+const CELL_SELECTED_FROM_RANGE = 'selected-cell';
+const CELL_SELECTED = 'selected-cell-border';
+const CELL_COPIED = 'selected-copied-cell';
+
 export default class ExcelTable extends LightningElement {
 
     columns = columns;
-    startSelection = false;
-    globalStartX;
-    globalEndX;
-    globalStartY;
-    globalEndY;
+    isRangeSelectionBegan = false;
+    isRendered = false;
+    hasCopiedItems = false;
+    //renderContextMenu = false;
+    globalCoordinates = {};
+    copyCoordinates = {};
 
     data = [
         {Id: '12', website: 'aa'},
@@ -45,127 +55,221 @@ export default class ExcelTable extends LightningElement {
         {Id: '12hj', website: 'bb'}
     ]
 
+    renderedCallback() {
+        if (this.isRendered) {
+            return;
+        }
+        this.hideContextMenu();
+        this.addContextMenuEventListenerToTable();
+        this.isRendered = true;
+    }
+
+    addContextMenuEventListenerToTable() {
+        this.template.querySelector('table').addEventListener('contextmenu', (e) => { 
+            e.preventDefault();
+
+            this.showContextMenu(); 
+            this.setContextMenuPosition(e.clientX, e.clientY);
+        }, false);
+    }
+
+    setContextMenuPosition(positionX, positionY) {
+        let contextMenuElement = this.template.querySelector('.menu-context');
+
+        contextMenuElement.style.setProperty("bottom", `${positionX}px`);
+        contextMenuElement.style.setProperty("right", `${positionY}px`);
+    }
+
+    showContextMenu() {
+        this.template.querySelector('.menu-context').style.display = "block";
+    }
+
+    hideContextMenu() {
+        this.template.querySelector('.menu-context').style.display = "none";
+    }
+
+    startRangeSelection() {
+        this.isRangeSelectionBegan = true;
+    }
+
+    stopRangeSelection() {
+        this.isRangeSelectionBegan = false;
+    }
+
+    setStartSelectionCoordinates(x, y) {
+        this.globalCoordinates.fromX = x;
+        this.globalCoordinates.fromY = y;
+    }
+
+    setEndSelectionCoordinates(x, y) {
+        this.globalCoordinates.toX = x;
+        this.globalCoordinates.toY = y;
+    }
+    // handlers 
+
+    handleContextMenuAction(e) {
+        switch (e.detail.name) {
+            case CONTEXT_MENU_ACTIONS.COPY:
+                this.applyCopyAction();
+                break;
+            case CONTEXT_MENU_ACTIONS.PASTE:
+                this.applyPasteAction();
+                break;
+        }
+    }
+
+    handleDoubleClick(e) {
+        console.log('Double')
+        e.target.disabled = false;
+    }
+
+    handleFocusOut(e) {
+        e.target.disabled = true;
+    }
+
     handleDown(e) {
-        this.clearSelectedRange();
-
-        this.startSelection = true;
-
-        this.globalStartX = e.currentTarget.dataset.row;
-        this.globalStartY = e.currentTarget.dataset.column;
+        if (e.which === 1) { //left click
+            this.clearSelectedRange();
+            this.cleareSelectedCell();
+            this.markSelectedCell(e.currentTarget);
+            this.startRangeSelection();
+            this.setStartSelectionCoordinates(
+                Number(e.currentTarget.dataset.row),
+                Number(e.currentTarget.dataset.column)
+            );
+        }
     }
 
     handleOver(e) {
-        if (this.startSelection) {
-            this.markSelectedRange(e.currentTarget.dataset.row,  e.currentTarget.dataset.column )
+        if (this.isRangeSelectionBegan) {
+            this.markSelectedRange(
+                Number(e.currentTarget.dataset.row), 
+                Number(e.currentTarget.dataset.column)
+            );
         }
     }
 
     handleUp(e) {
-        this.startSelection = false;
+        if (e.which === 1) { //left click
+            this.stopRangeSelection();
+            this.setEndSelectionCoordinates(
+                Number(e.currentTarget.dataset.row),
+                Number(e.currentTarget.dataset.column)
+            );
+        }
     }
 
-    clearSelectedRange() {
-        this.template.querySelectorAll('[data-selected="true"]').forEach(cell => {
-            cell.classList.remove('selected-cell');
-            cell.dataset.selected = false;
-        });
+    applyCopyAction() {
+        this.hideContextMenu();
+        this.clearCopiedCells();
+        this.makeCellsAsCopiedWithCssClassAndDatasetProperty(
+            this.transformCoordinates(
+                this.globalCoordinates.fromX, 
+                this.globalCoordinates.toX, 
+                this.globalCoordinates.fromY, 
+                this.globalCoordinates.toY
+            )
+        );
+        this.setItemToCopy();
     }
 
-    markSelectedRange(currentRow, currentColumn) {
+    applyPasteAction() {
+        console.log('globalCoordinantes: ' + JSON.stringify(this.globalCoordinates))
+        console.log('copyCoordinates:', JSON.stringify(this.copyCoordinates));
+        this.hideContextMenu();
+        this.clearCopiedCells();
+        this.clearItemsToCopy();
+    }
 
+    setItemToCopy() {
+        this.copyCoordinates = JSON.parse(JSON.stringify(this.globalCoordinates));
+        this.hasCopiedItems = true;
+    }
+
+    clearItemsToCopy() {
+        this.copyCoordinates = {};
+        this.hasCopiedItems = false;
+    }
+
+    clearCopiedCells() {
+        this.removeCssClassAndDatasetFromCellsBetweenRange(CELL_COPIED, 'copied');
+    }
+
+    markSelectedCell(cell) {
+        cell.classList.add(CELL_SELECTED);
+        cell.dataset.selectedcell = true;
+    }
+
+    cleareSelectedCell() {
+        this.removeCssClassAndDatasetFromCellsBetweenRange(CELL_SELECTED, 'selectedcell');
+    }
+
+    markSelectedRange(currentX, currentY) {
         this.clearSelectedRange();
-
-        let startX = this.globalStartX;
-        let endX = currentRow;
-
-        let startY = this.globalStartY;
-        let endY = currentColumn;
-
-        let coordinates = this.calculateCoordinates(startX, endX, startY, endY);
-
-        this.markCells(
-            { start: coordinates.startX, end: coordinates.endX }, 
-            { start: coordinates.startY, end: coordinates.endY }
+        this.markCellsAsSelectedWithCssClassAndDatasetProperty(
+            this.transformCoordinates(
+                this.globalCoordinates.fromX, 
+                currentX,
+                this.globalCoordinates.fromY, 
+                currentY
+            )
         );
     }
 
-
-    calculateCoordinates(startX, endX, startY, endY) {
-
-        let coordinates = {
-            startX: null,
-            endX: null,
-            startY: null,
-            endY: null
-        };
-
-        if (endX - startX <= 0 && endY - startY <= 0) {
-            coordinates.startX = endX;
-            coordinates.endX = startX;
-            coordinates.startY = endY;
-            coordinates.endY = startY;
-        } else  if (endX - startX <= 0 && endY - startY >= 0) {
-            coordinates.startX = endX;
-            coordinates.endX = startX;
-            coordinates.startY = startY;
-            coordinates.endY = endY;
-        } else  if (endX - startX >= 0 && endY - startY <= 0) {
-            coordinates.startX = startX;
-            coordinates.endX = endX;
-            coordinates.startY = endY;
-            coordinates.endY = startY;
-        } else  if (endX - startX >= 0 && endY - startY >= 0) {
-            coordinates.startX = startX;
-            coordinates.endX = endX;
-            coordinates.startY = startY;
-            coordinates.endY = endY;
-        }
-
-        return coordinates;
+    clearSelectedRange() {
+        this.setEndSelectionCoordinates(null, null);
+        this.removeCssClassAndDatasetFromCellsBetweenRange(CELL_SELECTED_FROM_RANGE, 'selected');
     }
 
+    transformCoordinates(startX, endX, startY, endY) {
+        return {
+            fromX: endX - startX > 0 ? startX : endX,
+            toX: endX - startX > 0 ? endX : startX,
+            fromY: endY - startY > 0 ? startY : endY,
+            toY: endY - startY > 0 ? endY : startY
+        };
+    }
 
-    markCells(x, y) {
-        let xList = this.generateX(x.start, x.end);
-        let yList = this.generateY(y.start, y.end);
+    markCellsAsSelectedWithCssClassAndDatasetProperty(coordinates) {
+        this.addCssClassAndDatasetToCellsBetweenRange(coordinates, CELL_SELECTED_FROM_RANGE, 'selected');
+    }
+
+    makeCellsAsCopiedWithCssClassAndDatasetProperty(coordinates) {
+        this.addCssClassAndDatasetToCellsBetweenRange(coordinates, CELL_COPIED, 'copied');
+    }
+
+    addCssClassAndDatasetToCellsBetweenRange({fromX, toX, fromY, toY}, cssClassToAdd, datasetPropertyToSet) {
+        let xList = this.getNumbersBetween(fromX, toX);
+        let yList = this.getNumbersBetween(fromY, toY);
 
         if (xList.length > 0 && yList.length > 0) {
             xList.forEach(x => {
                 yList.forEach(y => {
                     let cell = this.template.querySelector(`[data-row="${x}"][data-column="${y}"]`);
                     if (cell) {
-                        cell.classList.add('selected-cell');
-                        cell.dataset.selected = true;
+                        cell.classList.add(cssClassToAdd);
+                        cell.dataset[datasetPropertyToSet] = true;
                     }
-
                 });
             });
         }
     }
 
-    generateX(Xp, Xk) {
-        if (!Xp || !Xk) {
-            return [];
-        }
-
-        let xList = [];
-
-        for (let i = Xp; i <= Xk; i++) {
-            xList.push(i);
-        }
-        return xList;
+    removeCssClassAndDatasetFromCellsBetweenRange(cssClassToRemove, datasetPropertyToClear) {
+        this.template.querySelectorAll(`[data-${datasetPropertyToClear}="true"]`).forEach(cell => {
+            cell.classList.remove(cssClassToRemove);
+            cell.dataset[datasetPropertyToClear] = false;
+        });
     }
 
-    generateY(Yp, Yk) {
-        if (!Yp || !Yk) {
-            return [];
-        }
+    getNumbersBetween(a, b) {
+        let numbersInRange = [];
 
-        let yList = [];
-
-        for (let i = Yp; i <= Yk; i++) {
-            yList.push(i);
+        while (a <= b) {
+            numbersInRange.push(a);
+            a++;
         }
-        return yList;
+        return numbersInRange;
     }
+
 }
